04db208812fcdac45276e6f3235a87c1a1880417
Radu Gidei <radugidei@gmail.com>
2018-07-14 16:47:16 +0100

    move LineByCoordinates method to Geometry

--- Part 1 - ZeroTouch/Example 1 - Input Output/ZeroTouch/Geometry.cs
+++ Part 1 - ZeroTouch/Example 1 - Input Output/ZeroTouch/Geometry.cs
@@ -4,15 +4,31 @@ namespace DynamoWorkshop.ZeroTouch
 {
     public static class Geometry
     {
+        /// <summary>
+        /// Create a line from a pair of X,Y,Z coordinates
+        /// </summary>
+        /// <param name="X1">start point X</param>
+        /// <param name="Y1">start point Y</param>
+        /// <param name="Z1">start point Z</param>
+        /// <param name="X2">end point X</param>
+        /// <param name="Y2">end point Y</param>
+        /// <param name="Z2">end point Z</param>
+        /// <returns>A new line</returns>
         public static Line ByCoordinates(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
         {
-            using (var p1 = Point.ByCoordinates(X1, Y1, Z1))
-            {
-                using (var p2 = Point.ByCoordinates(X2, Y2, Z2))
-                {
-                    return Line.ByStartPointEndPoint(p1, p2);
-                }
-            }
+            // build the start/end Dynamo points from the coordinates
+            var startPoint = Point.ByCoordinates(X1, Y1, Z1);
+            var endPoint = Point.ByCoordinates(X2, Y2, Z2);
+
+            // use Dynamo API to build a line between these two points
+            var newLine = Line.ByStartPointEndPoint(startPoint, endPoint);
+
+            // IMPORTANT : don't forget to dispose of geometry you create in nodes !!
+            startPoint.Dispose();
+            endPoint.Dispose();
+
+            // return the created line
+            return newLine;
         }
     }
 }
