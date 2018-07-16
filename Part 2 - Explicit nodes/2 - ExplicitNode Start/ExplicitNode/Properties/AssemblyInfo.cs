c390567653399901dd4766740e49c12336675389
Radu Gidei <radugidei@gmail.com>
2018-07-16 17:54:08 +0100

    update P2E1 start to Dynamo 2.0

--- Part 2 - Explicit nodes/2 - ExplicitNode Start/ExplicitNode/Properties/AssemblyInfo.cs
+++ Part 2 - Explicit nodes/2 - ExplicitNode Start/ExplicitNode/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynamoUnchained.ZeroTouch")]
+[assembly: AssemblyTitle("DynamoWorkshop.ZeroTouch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DynamoUnchained.ZeroTouch")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamoWorkshop.ZeroTouch")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
